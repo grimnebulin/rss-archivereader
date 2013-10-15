9d40e744ed38f5c60fa982aa6b1ed0b72c7efec0
Sean McAfee <eefacm@gmail.com>
2013-10-14 17:13:46 -0700

    Adds missing GNU notice.

--- RSS/ArchiveReader/HtmlDocument.pm
+++ RSS/ArchiveReader/HtmlDocument.pm
@@ -1,5 +1,23 @@
 package RSS::ArchiveReader::HtmlDocument;
 
+# Copyright 2013 Sean McAfee
+
+# This file is part of RSS::ArchiveReader.
+
+# RSS::ArchiveReader is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# RSS::ArchiveReader is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with RSS::ArchiveReader.  If not, see
+# <http://www.gnu.org/licenses/>.
+
 use URI;
 use parent qw(HTML::TreeBuilder::XPath);
 use strict;
