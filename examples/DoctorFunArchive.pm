7c656293ac34fed6e699e5da560e116db182def6
Sean McAfee <eefacm@gmail.com>
2014-01-04 23:34:23 -0800

    Removes reference to obsolete attr_absolute method.

--- examples/DoctorFunArchive.pm
+++ examples/DoctorFunArchive.pm
@@ -18,7 +18,7 @@ sub render {
     my ($self, $doc) = @_;
     return map {
         $self->new_element(
-            'div', [ 'img', { src => $_->attr_absolute('href') } ]
+            'div', [ 'img', { src => $doc->resolve($_->attr('href')) } ]
         )
     } $doc->find('//a[img[contains(@src,"/thumbs/")]]');
 }
